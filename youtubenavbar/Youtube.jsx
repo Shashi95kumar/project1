dd4b795161fdf067b7131db12498fb99d0ef8499
Shashi95kumar <120559452+Shashi95kumar@users.noreply.github.com>
2023-03-04 22:58:06 +0530

    Add files via upload

---
+++ youtubenavbar/Youtube.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import "./youtube.css"
+
+
+const Youtube=()=>{
+  return(
+    
+    <div className="nav">
+    <div>
+      <img src="https://th.bing.com/th/id/OIP._0mJeL1mx94Kw7G2TkLu2QHaEK?w=329&h=185&c=7&r=0&o=5&pid=1.7" alt="" /></div>
+      <div className="block">
+        <ul>
+          <li>
+            <a href="">Cromecast</a></li>
+            <li>
+              <a href="">BellButton</a></li>
+              <li>
+                <a href="">Search</a></li>
+                <li><a href="">Account</a>
+              </li></ul></div></div>
+        
+  )
+
+}
+export default Youtube
