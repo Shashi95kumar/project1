ee67ad3de9f272d1d098a23298e2bef6b68868cc
Shashi95kumar <120559452+Shashi95kumar@users.noreply.github.com>
2023-03-06 19:37:44 +0530

    Add files via upload

---
+++ netflixnavbar/Netflix.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import "./netflix.css"
+
+
+const Netflix=()=>{
+  return(
+    
+    <div className="nav">
+    <div>
+      <img src="https://th.bing.com/th/id/OIP.fKRm_r6zsZWYKgkcdliHrgHaGL?w=203&h=180&c=7&r=0&o=5&pid=1.7https://th.bing.com/th/id/OIP.fKRm_r6zsZWYKgkcdliHrgHaGL?w=203&h=180&c=7&r=0&o=5&pid=1.7" alt="" /></div>
+      <div className="block">
+        <ul>
+          <li>
+            <a href="">HOME</a></li>
+            <li>
+              <a href="">MENU</a></li>
+              <li>
+                <a href="">OPTIONS</a></li>
+                <li><a href="">ABOUT</a>
+              </li>
+              <li><a href="">SETTINGS</a></li></ul></div></div>
+        
+  )
+
+}
+export default Netflix
