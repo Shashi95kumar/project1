2ef0de19ea0c958ded6546620dc793a14b829a16
Shashi95kumar <120559452+Shashi95kumar@users.noreply.github.com>
2023-03-13 20:39:24 +0530

    Add files via upload

---
+++ classbasedprops/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from"./Nav.jsx"
+import { Component } from "react";
+
+ class App extends Component{render()
+  {
+  return(
+<Nav girl="smita" pwd="123456" id="120" loc="mysore"/>
+  )
+}}
+export default App
