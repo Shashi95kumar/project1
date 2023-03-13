8e15d1be6d46de0815efd4cd3aafe2e56626edb7
Shashi95kumar <120559452+Shashi95kumar@users.noreply.github.com>
2023-03-13 20:39:40 +0530

    Add files via upload

---
+++ functionbasedprops/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import {Component}from "react"
+import Nav from "./Nav"
+class App extends Component{
+  render(){
+  return(
+    <Nav data="priya"/>
+  )
+}
+}
+export default App
